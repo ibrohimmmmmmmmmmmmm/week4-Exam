9ab47593b5a70033efbb01ccd12a66886229e231
Mukhtorov Ibrohim <mukhtorovibrohim209@gmail.com>
2026-05-03 20:16:23 +0500

    fix dependencies

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,14 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { HashRouter } from 'react-router-dom'
 import './index.css'
 import App from './App.jsx'
 import "./lib/i18n.js"
 import 'flag-icons/css/flag-icons.min.css';
 import "animate.css";
 
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <HashRouter>
-      <App />
-    </HashRouter>
+    <App />
   </StrictMode>,
 )
